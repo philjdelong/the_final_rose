36af708901962e97221dcf10db3007e85eae08b5
Phil DeLong <philjdelong@gmail.com>
2019-12-30 17:11:08 -0700

    Complete story 6 and pass all model tests

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -6,19 +6,7 @@ class Bachelorette <ApplicationRecord
   validates_presence_of :season_description
 
   def contestant_avg_age
-    (contestant_total_age/contestant_total_count.to_f).round(2)
-  end
-
-  def contestant_total_age
-    x = 0
-    contestants.each do |contestant|
-      x += contestant.age
-    end
-    return x
-  end
-
-  def contestant_total_count
-    contestants.length
+    contestants.average(:age).to_f.round(2)
   end
 
   def unique_hometowns
